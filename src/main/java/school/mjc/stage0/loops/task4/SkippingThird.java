3d835dd586a77cc9a77f6331327bb7acb7e10e4d
pomidorNagolove <harrier136@icloud.com>
2023-05-07 23:22:59 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
+        if (lastPrinted > 0) {
+            for (int i = 1; i <= lastPrinted; i++) {
+                if (i % 3 != 0) {
+                    System.out.println(i);
+                } else {
+                    System.out.print("");
+                }
+            }
+        } else {
+            System.out.print("");
+        }
 
     }
 }
